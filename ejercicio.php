a2f4a540d4efb158727e995ff110857db804c3e0
Josep Ingles Arenes <joinar@floridauniversitaria.es>
2016-09-27 13:10:00 +0200

    programacion

---
+++ ejercicio.php
@@ -0,0 +1,17 @@
+<?php
+//fichero index.php
+//creado por Paco Gomez
+?>
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+      
+  </head>
+  <body>
+  <?php
+        echo "Josep<br>";
+        echo "Ingles<br>";
+        print "Arenes<br>";
+        ?>
+        </body>
+</html>
